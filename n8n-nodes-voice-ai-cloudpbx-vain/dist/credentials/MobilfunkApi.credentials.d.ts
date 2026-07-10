263e95cf756d05d83e1aad4086eb1535d0f469f7
André Rink <andre.rink@telekom.de>
2026-07-10 15:10:58 +0200

    Remove Mobile Auth Code

--- n8n-nodes-voice-ai-cloudpbx-vain/dist/credentials/MobilfunkApi.credentials.d.ts
+++
@@ -1,9 +0,0 @@
-import { ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
-export declare class MobilfunkApi implements ICredentialType {
-    name: string;
-    displayName: string;
-    icon: "file:voiceAINotesWebhook.png";
-    properties: INodeProperties[];
-    test: ICredentialTestRequest;
-}
-//# sourceMappingURL=MobilfunkApi.credentials.d.ts.map
